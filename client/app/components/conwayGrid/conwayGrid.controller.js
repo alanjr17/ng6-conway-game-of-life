21d47bb802bf4d7d6eb39a45ae8524c922eab316
Alan Omar Martinez <alan.martinez@globant.com>
2018-05-17 09:55:12 -0500

    [conwayGrid] Create an initGrid Function to create an array of arrays with the given sizes. Render in HTML a table based on the grid array

--- client/app/components/conwayGrid/conwayGrid.controller.js
+++ client/app/components/conwayGrid/conwayGrid.controller.js
@@ -5,8 +5,34 @@ class ConwayGridController {
     this.$scope = $scope;
 
     this.$scope.$on('generateGrid', function(event, data) {
-      console.log('Received Params: ', _this.rowSize, _this.colSize);
+      _this.initGrid();
     });
+
+    this.grid = [];
+  }
+
+  /**
+   * Generate an array of arrays to define a new grid with component params sizes
+   */
+  initGrid() {
+    this.grid = [];
+    for(let row = 0; row < this.rowSize; row++) {
+      let currentRow = [];
+      for(let cell = 0; cell < this.colSize; cell++) {
+        currentRow.push(this.getEmptyCell());
+      }
+      this.grid.push(currentRow);
+    }
+  }
+
+  /**
+   * Define an structure for Cells in grid that will be changed by the game loops
+   */
+  getEmptyCell() {
+    return {
+      alive: false,
+      willLive: false
+    }
   }
 }
 
