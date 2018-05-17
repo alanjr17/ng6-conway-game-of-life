a0df0515fe704ab3d3d243e86c6151c2d1ce1186
Alan Omar Martinez <alan.martinez@globant.com>
2018-05-17 10:10:18 -0500

    [conwayGrid] Add buttons to interface for Start, Stop and Reset. Define a default init seeds

--- client/app/components/conwayGrid/conwayGrid.controller.js
+++ client/app/components/conwayGrid/conwayGrid.controller.js
@@ -6,9 +6,11 @@ class ConwayGridController {
 
     this.$scope.$on('generateGrid', function(event, data) {
       _this.initGrid();
+      _this.setDefaultSeeds();
     });
 
     this.grid = [];
+    this.iteration = 0;
   }
 
   /**
@@ -23,6 +25,32 @@ class ConwayGridController {
       }
       this.grid.push(currentRow);
     }
+  }
+
+  startGame() {
+    console.log('Start Game');
+  }
+
+  stopGame() {
+    console.log('Stop Game');
+  }
+
+  /**
+   * Clear all Grid with empty values, reset iterator count and set Default seeds.
+   */
+  resetGame() {
+    this.initGrid();
+    this.setDefaultSeeds();
+    this.iteration = 0;
+  }
+
+  setDefaultSeeds(){
+    this.grid[0][0].alive = true;
+    this.grid[0][1].alive = true;
+    this.grid[1][0].alive = true;
+    this.grid[1][3].alive = true;
+    this.grid[2][1].alive = true;
+    this.grid[2][2].alive = true;
   }
 
   /**
