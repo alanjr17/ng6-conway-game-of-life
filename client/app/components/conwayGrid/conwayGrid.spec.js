4f667efd784ad9362a69376dfcaeec4a9f07b188
Alan Omar Martinez <alan.martinez@globant.com>
2018-05-17 10:26:07 -0500

    [conwayGridSpec] Define Tests for decideFuture function, which check neighbors and APPLY GAME RULES

--- client/app/components/conwayGrid/conwayGrid.spec.js
+++ client/app/components/conwayGrid/conwayGrid.spec.js
@@ -38,6 +38,33 @@ describe('ConwayGrid', () => {
       expect(controller.grid[0].length).toEqual(50);
     });
 
+    it('evaluate neighbors in the correct way', () => {
+      const controller = makeController();
+      controller.rowSize = 10;
+      controller.colSize = 10;
+      controller.initGrid();
+      controller.setDefaultSeeds();
+
+      expect(controller.decideCellFuture(0,0)).toBe(true);
+      expect(controller.decideCellFuture(1,1)).toBe(false);
+
+      controller.grid[1][4].alive = true;
+      expect(controller.grid[2][3].alive).toBe(false);
+      expect(controller.decideCellFuture(2,3)).toBe(true);
+    });
+
+    it('reborn cells in the correct way', () => {
+      const controller = makeController();
+      controller.rowSize = 10;
+      controller.colSize = 10;
+      controller.initGrid();
+      controller.setDefaultSeeds();
+
+      controller.grid[1][4].alive = true;
+      expect(controller.grid[2][3].alive).toBe(false);
+      expect(controller.decideCellFuture(2,3)).toBe(true);
+    });
+
   });
 
   describe('Component', () => {
